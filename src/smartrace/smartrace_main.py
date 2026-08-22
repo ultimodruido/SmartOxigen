d61128a56383b6e5ea2ef2ffd8f0da57403ea30c
ultimodruido <public@nicolinux.it>
2026-08-22 23:06:13 +0200

    file syntax clean up

--- src/smartrace/smartrace_main.py
+++ src/smartrace/smartrace_main.py
@@ -12,6 +12,7 @@ state machine:
   - catch LostConnection exception
   - read messages in async and emit corresponding signal
 """
+
 import asyncio
 import json
 
@@ -35,8 +36,8 @@ async def smartrace_send(connection:ClientConnection):
         except ConnectionClosed:
             print("Smartrace sending: interrupted")
 
-
         await asyncio.sleep(1)
+
 
 managed_events = {
     "update_controller_data": smartrace_events.player_list,
@@ -46,9 +47,10 @@ managed_events = {
     "update_controller_state": smartrace_events.update_controller_state,
 }
 
-async def smartrace_receive(connection:ClientConnection):
+
+async def smartrace_receive(connection: ClientConnection):
     async for message in connection:
-        #print("### tablet2sensor ###")
+        # print("### tablet2sensor ###")
 
         try:
             print(message)
@@ -58,26 +60,28 @@ async def smartrace_receive(connection:ClientConnection):
             try:
                 managed_events[message_json["type"]].emit(message_json["data"])
             except KeyError:
-                print('message received but skipped key error')
-
+                print("message received but skipped key error")
 
         except ConnectionClosed:
             print("smartrace_receive ConnectionClosed exception")
-            #messages_queue2sensor.append(message)
+            # messages_queue2sensor.append(message)
             break
 
-async def smartrace_connect(server:str, port:str) -> None:
-    print('async smartrace_connect started')
+
+async def smartrace_connect(server: str, port: str) -> None:
+    print("async smartrace_connect started")
     async for smartrace_socket in connect(f"ws://{server}:{port}"):
         try:
             smartrace_events.connection_successful.emit()
             _messages_queue.append('{"type": "api_version"}')
-            _messages_queue.append('{"type": "controller_set", "data": {"controller_id": "X"}}')
+            _messages_queue.append(
+                '{"type": "controller_set", "data": {"controller_id": "X"}}'
+            )
             try:
                 tasks_list = [
                     smartrace_send(smartrace_socket),
-                    smartrace_receive(smartrace_socket)
-                    ]
+                    smartrace_receive(smartrace_socket),
+                ]
                 await asyncio.gather(*tasks_list)
             except TypeError:
                 smartrace_events.connection_closed.emit()
